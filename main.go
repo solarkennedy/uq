59539c9c780165d25b588b5c0e02295ca39ca231
Kyle Anderson <kyle@xkyle.com>
2016-03-10 20:55:19 -0800

    Isolate input and output into functions

--- main.go
+++ main.go
@@ -8,23 +8,30 @@ import (
 	"github.com/ghodss/yaml"
 )
 
-func main() {
-
-	stat, _ := os.Stdin.Stat()
+func readData() []byte {
 	var bytes []byte
+	stat, _ := os.Stdin.Stat()
 	if (stat.Mode() & os.ModeCharDevice) == 0 {
 		bytes, _ = ioutil.ReadAll(os.Stdin)
 	} else {
-		//file, _ := os.Open(os.Args[len(os.Args)-1])
 		bytes, _ = ioutil.ReadFile(os.Args[len(os.Args)-1])
 	}
+	return bytes
+}
 
-	json, err := yaml.YAMLToJSON(bytes)
+func outputData(data []byte) int {
+	json, err := yaml.YAMLToJSON(data)
 	if err != nil {
 		fmt.Printf("err: %v\n", err)
-		return
+		return 1
 	} else {
 		fmt.Println(string(json))
 	}
+	return 0
+}
 
+func main() {
+	data := readData()
+	exitcode := outputData(data)
+	os.Exit(exitcode)
 }
