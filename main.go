e9ea91a117e8f40950177cdedf5236127280db57
Kyle Anderson <kyle@xkyle.com>
2016-03-10 21:51:35 -0800

    Try using docopt

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"os"
 
+	"github.com/docopt/docopt-go"
 	"github.com/ghodss/yaml"
 )
 
@@ -26,11 +27,37 @@ func outputData(data []byte) int {
 		return 1
 	} else {
 		fmt.Println(string(json))
+		return 0
 	}
-	return 0
+}
+
+func parseArgs() map[string]interface{} {
+	usage := `uq - Universal serialized data reader to JSON
+
+Usage:
+  uq
+  uq <file>
+  uq --yaml
+  uq -h | --help
+  uq --version
+
+Options:
+  -h --help     Show this screen
+  --version     Show version
+  -y --yaml     Force reading input as YAML
+
+Examples:
+  cat input.yaml | uq | jq .
+  jq --yaml input.yaml | jq .
+`
+
+	arguments, _ := docopt.Parse(usage, nil, true, "0.0.1", false)
+	fmt.Println(arguments)
+	return arguments
 }
 
 func main() {
+	parseArgs()
 	data := readData()
 	exitcode := outputData(data)
 	os.Exit(exitcode)
